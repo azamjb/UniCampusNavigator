839fbca0fd3cc55d2f2ab7abc29b53b94325516a
Azam Jawad Butt <azamjb123@gmail.com>
2023-04-04 01:39:30 -0400

    Added popup feature to POI's

--- src/POIMarker.java
+++ src/POIMarker.java
@@ -1,8 +1,10 @@
 import java.io.IOException;
-
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 // https://stackoverflow.com/questions/14705684/placing-a-marker-within-the-image
 public class POIMarker extends JLabel {
@@ -12,5 +14,16 @@ public class POIMarker extends JLabel {
         } catch (IOException ex) {
             ex.printStackTrace();
         }
+
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                showPopup(e);
+            }
+        });
+    }
+
+    private void showPopup(MouseEvent e) {
+        JOptionPane.showMessageDialog(null, "Information about this POI", "POI Info", JOptionPane.INFORMATION_MESSAGE);
     }
 }
